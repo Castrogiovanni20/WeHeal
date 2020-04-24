3821b1958a85142fb1d8a5f18433a7694e54b2c8
Your Name <barbibarni@gmail.com>
2020-04-24 14:27:08 -0300

    layout activityInicial y layout registro

--- app/src/main/java/com/example/weheal/MainActivity.java
+++ app/src/main/java/com/example/weheal/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.weheal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button registro = findViewById(R.id.registro);
+        Button login = findViewById(R.id.login);
+
+
+
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Registro.class);
+                startActivity(intent);
+
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
+
---
+++ app/src/main/java/com/example/weheal/Registro.java
@@ -0,0 +1,29 @@
+package com.example.weheal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+
+        Button principal = findViewById(R.id.paginaPrincipal);
+
+        principal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
