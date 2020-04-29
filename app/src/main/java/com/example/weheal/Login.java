421be1af007aa84d7b1d98cd0e03f42e049736f7
CastrogiovanniRami <38988159+Castrogiovanni20@users.noreply.github.com>
2020-04-29 18:50:30 -0300

    Validaciones en el registro

--- app/src/main/java/com/example/weheal/Login.java
+++ app/src/main/java/com/example/weheal/Login.java
@@ -7,17 +7,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    private EditText Email, Password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Email = (EditText) findViewById(R.id.input_email);
+        Password = (EditText) findViewById(R.id.input_password);
 
         Button principal = findViewById(R.id.paginaPrincipal);
 
@@ -30,6 +38,22 @@ public class Login extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void validarLogin(View v){
+        validarEditText(Email, "Email");
+        validarEditText(Password, "Password");
+    }
+
+    public void validarEditText(EditText element, String nombre){
+        if(element.getText().toString().isEmpty()){
+            element.getBackground().setColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP);
+            element.setHintTextColor(Color.RED);
+            element.setError(nombre + " incompleto");
+        } else {
+            element.getBackground().setColorFilter(Color.BLACK, PorterDuff.Mode.SRC_ATOP);
+            element.setHintTextColor(Color.BLACK);
+        }
     }
 }
 
--- app/src/main/java/com/example/weheal/Registro.java
+++ app/src/main/java/com/example/weheal/Registro.java
@@ -3,16 +3,28 @@ package com.example.weheal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Registro extends AppCompatActivity {
+
+    private EditText Usuario, Email, Password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        Usuario = (EditText) findViewById(R.id.input_usuario);
+        Email = (EditText) findViewById(R.id.input_email);
+        Password = (EditText) findViewById(R.id.input_password);
 
         Button principal = findViewById(R.id.paginaPrincipal);
 
@@ -24,6 +36,28 @@ public class Registro extends AppCompatActivity {
 
             }
         });
+    }
 
+    public void validarRegistro(View v){
+        validarEditText(Usuario, "Usuario");
+        validarEditText(Email, "Email");
+        validarEditText(Password, "Password");
+    }
+
+    public void validarEditText(EditText element, String nombre){
+        if(element.getText().toString().isEmpty()){
+            element.getBackground().setColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP);
+            element.setHintTextColor(Color.RED);
+            element.setError(nombre + " incompleto");
+        } else {
+            if ((element.getText().toString().length() < 6)) {
+                element.getBackground().setColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP);
+                element.setHintTextColor(Color.RED);
+                element.setError(nombre + " debe ser mayor a 6 caracteres");
+            } else {
+                element.getBackground().setColorFilter(Color.BLACK, PorterDuff.Mode.SRC_ATOP);
+                element.setHintTextColor(Color.BLACK);
+            }
+        }
     }
 }
