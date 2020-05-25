07da713e75df90bb0659ce23e1e9c25abe83cbdb
CastrogiovanniRami <38988159+Castrogiovanni20@users.noreply.github.com>
2020-05-24 22:35:22 -0300

    Animacion al buscar insumo

--- app/src/main/java/com/example/weheal/MenuActivity.java
+++ app/src/main/java/com/example/weheal/MenuActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.animation.Animator;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,6 +20,7 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.facebook.login.LoginManager;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -36,6 +40,7 @@ public class MenuActivity extends AppCompatActivity {
     RecyclerView mRecyclerView;
     FirebaseDatabase firebaseDatabase;
     DatabaseReference reference;
+    LottieAnimationView loading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +51,8 @@ public class MenuActivity extends AppCompatActivity {
 
         search      = findViewById(R.id.search);
         searchField = findViewById(R.id.search_field);
+
+        loading = findViewById(R.id.loading);
 
         mRecyclerView = findViewById(R.id.recyclerview);
         mRecyclerView.setHasFixedSize(true);
@@ -77,6 +84,8 @@ public class MenuActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 firebaseSearch(searchField.getText().toString());
+                mostrarAnimacionLoading();
+                loading.playAnimation();
             }
         });
     }
@@ -155,6 +164,35 @@ public class MenuActivity extends AppCompatActivity {
             textBienvenida = findViewById(R.id.texto_bienvenida);
             textBienvenida.setText("¡Bienvenido " + user.getDisplayName() + "!");
         }
+    }
+
+    private void mostrarAnimacionLoading(){
+        mRecyclerView.setVisibility(View.INVISIBLE);
+        loading.setVisibility(View.VISIBLE);
+
+        loading.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator loading) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                loading.setVisibility(View.INVISIBLE);
+                mRecyclerView.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+
     }
 
 
